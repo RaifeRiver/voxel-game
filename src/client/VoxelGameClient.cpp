237feaa5713330b07e620c668863df5dc03b840b
RaifeRiver <cade@blazestone.io>
2026-08-12 16:46:45 +0100

    Run command queue and systems every frame

--- src/client/VoxelGameClient.cpp
+++ src/client/VoxelGameClient.cpp
@@ -6,15 +6,23 @@
 #include "window/glfw/GLFWWindow.h"
 
 namespace voxel_game::client {
-	void load(ecs::ECSRegistry &registry) {
+	void load(ecs::ECSRegistry& registry) {
 		registry.addResource<window::Window, window::glfw::GLFWWindow>(window::glfw::GLFWWindow("Voxel Game", true, 0, 0));
 		registry.addResource<render::RenderEngine, render::vulkan::VulkanEngine>(render::vulkan::VulkanEngine(registry));
 	}
 
 	void run(ecs::ECSRegistry &registry) {
 		auto& window = registry.getResource<window::Window>();
+
+		const std::chrono::time_point<std::chrono::system_clock> lastTime = std::chrono::high_resolution_clock::now();
 		while (!window.shouldClose()) {
+			std::chrono::time_point<std::chrono::system_clock> currentTime = std::chrono::high_resolution_clock::now();
+			const float deltaTime = std::chrono::duration<float>(currentTime - lastTime).count();
+
 			window.pollEvents();
+
+			registry.getCommandQueue().execute(registry);
+			registry.getSystemManager().runSystems(registry, deltaTime);
 		}
 	}
 

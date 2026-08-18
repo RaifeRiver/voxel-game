f48907f01dc2f46720bfc29ac9c6cf8570d4c9f7
RaifeRiver <cade@blazestone.io>
2026-08-18 10:48:14 +0100

    Add FileHelper for reading files

---
+++ src/common/util/FileHelper.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <fstream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+namespace voxel_game::util {
+	template <typename T> std::vector<T> readFile(const std::string& path) {
+		std::ifstream file(path, std::ios::ate | std::ios::binary);
+		if (!file.is_open()) {
+			throw std::runtime_error("Failed to open file: " + path);
+		}
+		const size_t fileSize = file.tellg();
+		std::vector<T> buffer(fileSize / sizeof(T));
+		file.seekg(0);
+		file.read(reinterpret_cast<char*>(buffer.data()), fileSize);
+		file.close();
+		return buffer;
+	}
+}

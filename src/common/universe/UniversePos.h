454277f8af94f548126ea5e038e321c61233f171
RaifeRiver <cade@blazestone.io>
2026-08-28 17:37:57 +0100

    Add extra functions to UniversePos

--- src/common/universe/UniversePos.h
+++ src/common/universe/UniversePos.h
@@ -3,10 +3,39 @@
 #include "glm/vec3.hpp"
 
 namespace voxel_game::universe {
-	static uint32_t SECTOR_SIZE = 64;
+	constexpr static int32_t SECTOR_SIZE = 64;
 
 	struct UniversePos {
 		glm::i64vec3 sector;
 		glm::vec3 local;
+
+		void fix() {
+			const auto iLocal = glm::ivec3(local);
+			const glm::ivec3 sectors = iLocal / SECTOR_SIZE;
+			if (sectors != glm::ivec3(0)) {
+				sector += sectors;
+				local -= sectors * SECTOR_SIZE;
+			}
+		}
+
+		[[nodiscard]] double length() const {
+			return glm::length(glm::dvec3(sector)) * SECTOR_SIZE + glm::length(local);
+		}
 	};
+
+	[[nodiscard]] inline UniversePos operator+(const UniversePos& a, const UniversePos& b) {
+		UniversePos result = {};
+		result.sector = a.sector + b.sector;
+		result.local = a.local + b.local;
+		result.fix();
+		return result;
+	}
+
+	[[nodiscard]] inline UniversePos operator-(const UniversePos& a, const UniversePos& b) {
+		UniversePos result = {};
+		result.sector = a.sector - b.sector;
+		result.local = a.local - b.local;
+		result.fix();
+		return result;
+	}
 }
